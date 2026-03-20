2fc1786cd707cfa6f221b1cad3bd58126fcbb3fa
gowthaaman <gokrishna98@gmail.com>
2026-03-20 14:03:33 +0530

    des

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const PaytmChecksum = require('paytmchecksum');
-const axios = require('axios');
 
 const {
   PAYTM_MERCHANT_ID,
